e99397db3f90f5881d31304f6bf0d0a4826fd7fd
fnpetersv <fnpetersv@gmail.com>
2022-08-22 10:55:52 -0400

    feat: remove login

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -91,12 +91,7 @@ export default function Example() {
               </div>
               <div className="hidden md:absolute md:flex md:items-center md:justify-end md:inset-y-0 md:right-0">
                 <span className="inline-flex rounded-md shadow">
-                  <a
-                    href="#"
-                    className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50"
-                  >
-                    Log in
-                  </a>
+                  
                 </span>
               </div>
             </nav>
@@ -142,12 +137,7 @@ export default function Example() {
                     </a>
                   ))}
                 </div>
-                <a
-                  href="#"
-                  className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
-                >
-                  Log in
-                </a>
+                
               </div>
             </Popover.Panel>
           </Transition>
