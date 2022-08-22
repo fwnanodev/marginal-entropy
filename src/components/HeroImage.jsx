1e8663ff4317f4ba185fc0b871f6cf608711328b
fnpetersv <fnpetersv@gmail.com>
2022-08-22 10:53:04 -0400

    feat: remove get started and live demo

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -165,20 +165,10 @@ export default function Example() {
             </p>
             <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
               <div className="rounded-md shadow">
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
-                >
-                  Get started
-                </a>
+                
               </div>
               <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
-                >
-                  Live demo
-                </a>
+                
               </div>
             </div>
           </div>
