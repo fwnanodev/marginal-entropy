ce6091583860adb45b023bfa500c0d21df56d641
jrthartwig <jocelynnhartwig@gmail.com>
2023-01-29 13:25:34 -0500

    refactor: landing page layout

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,19 +1,15 @@
 import Biographies from "../components/Biographies";
 import ClassInfo from "../components/ClassInfo";
-import Footer from "../components/Footer";
-import HeroImage from "../components/HeroImage";
 import MissionHero from "../components/MissionHero";
 import SignUp from "../components/SignUp";
 
 const LandingPage = () => {
   return (
     <>
-      {/* <HeroImage /> */}
       <MissionHero />
       <ClassInfo />
       <Biographies />
       <SignUp />
-      {/* <Footer /> */}
     </>
   );
 };
