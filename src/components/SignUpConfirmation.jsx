6b8b37ee8ce98bc9e11ce2833b97a31caff831a2
jrthartwig <jocelynnhartwig@gmail.com>
2023-01-31 12:56:01 -0500

    fix: margin on sign up confirmation

--- src/components/SignUpConfirmation.jsx
+++ src/components/SignUpConfirmation.jsx
@@ -1,22 +1,24 @@
 const SignUpConfirmation = () => {
   return (
     <div className="flex justify-center mb-96 border-t-2 border-siennaRed">
-      <div className="m-8 space-y-12">
-        <h1 className="text-5xl mt-20 text-blue text-center">
+      <div>
+        <h1 className="text-5xl mt-20 text-blue text-center mx-12">
           Thank you for signing up for our Minecraft Coding Class!
         </h1>
-        <div className="space-y-6 text-3xl text-slateGray text-center">
-          <div>A Minecraft Educator will be in touch with you via email.</div>
-          <div>
-            <b>
-              If you have any questions in the meantime please reach out to us
-              at jocelynn@marginalentropy.org
-            </b>
-            <div className="mt-12 text-2xl text-siennaRed hover:underline">
-              <a href="/">Return Home</a>
-            </div>
+        {/* <div className="text-3xl text-slateGray text-center"> */}
+        <div className="text-3xl text-slateGray text-center mx-12 mt-12">
+          A Minecraft Educator will be in touch with you via email.
+        </div>
+        <div className="mx-12 text-2xl mt-12 text-center text-blue">
+          <b>
+            If you have any questions in the meantime please reach out to us at
+            contact@marginalentropy.org
+          </b>
+          <div className="mt-12 text-2xl text-siennaRed hover:underline">
+            <a href="/">Return Home</a>
           </div>
         </div>
+        {/* </div> */}
       </div>
     </div>
   );
